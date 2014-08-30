eaaca36698409b873bb52c448475a06fd6c72661
simon-ramsay <simon@rangle.io>
2014-08-30 12:48:59 -0400

    updated widget settings ui components dist

--- libs/widget-settings-ui-components/dist/js/angular/url-field.js
+++ libs/widget-settings-ui-components/dist/js/angular/url-field.js
@@ -45,6 +45,12 @@
           // Validation state
           scope.valid = true;
 
+          scope.$watch("url", function (url) {
+            if (url && scope.doValidation) {
+              scope.valid = testUrl(scope.url);
+            }
+          });
+
           scope.$watch("valid", function (valid) {
             if (!valid) {
               scope.failCount += 1;
@@ -63,12 +69,6 @@
               scope.valid = true;
             }
           });
-
-          scope.onChange = function () {
-            if (scope.doValidation) {
-              scope.valid = testUrl(scope.url);
-            }
-          };
         }
       };
     }]);
@@ -84,7 +84,7 @@ app.run(["$templateCache", function($templateCache) {
     "  <div class=\"checkbox\">\n" +
     "    <label>\n" +
     "      <input name=\"validate-url\" ng-click=\"doValidation = !doValidation\" type=\"checkbox\"\n" +
-    "             value=\"validate-url\" checked=\"checked\"> {{\"url.validate\" | translate}}\n" +
+    "             value=\"validate-url\" checked=\"checked\"> {{\"url.validate.label\" | translate}}\n" +
     "    </label>\n" +
     "    <span popover=\"{{'url.validate.tooltip' | translate}}\" popover-trigger=\"click\"\n" +
     "          popover-placement=\"top\" rv-tooltip></span>\n" +
@@ -95,7 +95,7 @@ app.run(["$templateCache", function($templateCache) {
     "</div>\n" +
     "<div class=\"form-group\">\n" +
     "  <label>{{ \"url.label\" | translate }}</label>\n" +
-    "  <input name=\"url\" type=\"text\" ng-model=\"url\" class=\"form-control\" placeholder=\"http://\" ng-change=\"onChange()\">\n" +
+    "  <input name=\"url\" type=\"text\" ng-model=\"url\" class=\"form-control\" placeholder=\"http://\">\n" +
     "</div>\n" +
     "");
 }]);
