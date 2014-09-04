c8febabbd24a93c4c5ce1e0b33858269a780a16e
jenkins-rise <jenkins@risevision.com>
2014-09-04 17:09:02 -0400

    Update bower libs

--- libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
+++ libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
@@ -137,6 +137,7 @@ angular.module('risevision.widget.common')
       var newSettings = angular.copy(settings);
 
       delete newSettings.params.id;
+      delete newSettings.params.companyId;
       delete newSettings.params.rsW;
       delete newSettings.params.rsH;
 
