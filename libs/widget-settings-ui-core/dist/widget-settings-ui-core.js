45c2c013f9df43227b3e4ca242d25992db23961f
jenkins-rise <jenkins@risevision.com>
2014-09-08 14:29:22 -0400

    Update bower libs

--- libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
+++ libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
@@ -254,3 +254,35 @@ angular.module('risevision.widget.common')
   }])
 
   .value('defaultSettings', {});
+
+(function (angular) {
+  'use strict';
+
+  angular.module('risevision.widget.common.visualization')
+    .factory('visualizationApi', ['$q', '$window', function ($q, $window) {
+      var deferred = $q.defer();
+      var promise;
+
+      var factory = {
+        get: function () {
+          if (!promise) {
+            promise = deferred.promise;
+            if (!$window.google.visualization) {
+              $window.google.setOnLoadCallback(function () {
+                deferred.resolve($window.google.visualization);
+              });
+            }
+            else {
+              deferred.resolve($window.google.visualization);
+            }
+          }
+          return promise;
+        }
+      };
+      return factory;
+
+    }]);
+
+})(angular);
+
+
