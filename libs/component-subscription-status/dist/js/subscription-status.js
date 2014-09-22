6284dd35d3a33911fc858b480829979d15a6ac07
jenkins-rise <jenkins@risevision.com>
2014-09-22 16:25:10 -0400

    Update bower libs

--- libs/component-subscription-status/dist/js/subscription-status.js
+++ libs/component-subscription-status/dist/js/subscription-status.js
@@ -2,7 +2,7 @@
   "use strict";
 
   angular.module("risevision.widget.common.subscription-status.config", [])
-    .value("STORE_URL", "http://store.risevision.com/~rvi/store/")
+    .value("STORE_URL", "https://store.risevision.com/")
     .value("IN_RVA_PATH", "?up_id=iframeId&parent=parentUrl#/product/productId/?inRVA&cid=companyId")
     .value("STORE_SERVER_URL", "https://store-dot-rvaserver2.appspot.com/")
     .value("PATH_URL", "v1/company/companyId/product/status?pc=")
