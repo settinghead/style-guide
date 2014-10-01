b93c94fa9188f4fa3794d1d0bbfdfccea19e120d
jenkins-rise <jenkins@risevision.com>
2014-10-01 10:31:27 -0400

    Update bower libs

--- libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
+++ libs/widget-settings-ui-core/dist/widget-settings-ui-core.js
@@ -24,6 +24,7 @@ angular.module("risevision.widget.common")
     $scope.loadAdditionalParams = function () {
       settingsGetter.getAdditionalParams().then(function (additionalParams) {
         $scope.settings.additionalParams = additionalParams;
+        $scope.$broadcast("loadAdditionalParams", additionalParams);
       },
       function (err) {alert (err); });
     };
